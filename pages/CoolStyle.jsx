459facd888aa97a8665f019b33e19246c2848919
juanmagal10 <juanmagalvan.24@gmail.com>
2022-06-11 10:12:23 -0300

    upload for the first render test

--- pages/CoolStyle.jsx
+++ pages/CoolStyle.jsx
@@ -4,12 +4,13 @@ import Link from 'next/link';
 import CoolStyleImg from '../public/assets/projects/coolStyle.jpg';
 import { RiRadioButtonFill } from 'react-icons/ri';
 
+
 const CoolStyle = () => {
   return (
     <div className='w-full'>
           <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
               <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-              <Image className='absolute z-1' layout='fill' objectFit='cover' src={CoolStyleImg} /> 
+              <Image className='absolute z-1' layout='fill' objectFit='cover' src={CoolStyleImg} alt='portada' /> 
               <div className='absolute top-[70%] max-w-[1240] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-3 '>
                   <h2 className='py-2'>Clothes Store Home</h2>
                   <h3>Js / CSS / HTML / Axios</h3>
